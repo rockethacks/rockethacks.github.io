63cdf561e19e34be79d3a84b021ee69af27158f4
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-13 19:40:11 -0500

    Add Apply Button in Hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,5 +1,7 @@
 // src/components/hero/Hero.tsx
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const Hero = () => {
   // Add connection verification on component mount
@@ -20,12 +22,27 @@ export const Hero = () => {
       >
         <source src="/blackhole.webm" type="video/webm" />
       </video>
-      <div className="relative py-64 md:py-48">
+      <div className="relative py-44 md:py-36">
         <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500 text-center text-balance m-1 md:m-28 lg:m-16 text-4xl lg:text-6xl xl:text-8xl">
-          Join the biggest <br className="hidden xl:block"/> hackathon in the midwest!
+          Join the biggest <br className="hidden xl:block" /> hackathon in the
+          midwest!
         </h1>
         <br />
       </div>
+      <Link href="https://portal.rockethacks.org">
+        <Button
+          variant="default"
+          className={`bg-transparent text-white rounded-md
+               border-2 border-[#FFDA00]
+               transform hover:scale-105
+               transition-all duration-300
+               backdrop-blur-sm
+               hover:bg-[#FFDA00]/10 text-md md:text-2xl p-4 md:p-7
+               `}
+        >
+          Apply Now
+        </Button>
+      </Link>
     </section>
   );
 };
