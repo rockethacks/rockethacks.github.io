44619b1d13d57ea5be1d3fa2759f6be421cb8d1c
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-18 01:24:49 -0500

    Update About

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,12 +5,18 @@ export default function about() {
     <div>
       <section
         id="about"
-        className="about-us bg-white text-black py-16 px-5 md:px-10 xl:py-20 xl:px-[300px]"
+        className="about-us bg-[#030c1b] text-white py-16 px-5 md:px-10 xl:py-20 xl:px-[300px]"
       >
         <div className="container text-left">
           <h2 className="text-3xl font-bold my-[10px]">ABOUT US</h2>
-          <p className="mt-4 text-lg">
-            This is the About Us section. Add your content here.
+          <p className="mt-4 text-base text-balance break-normal font-normal">
+            RocketHacks is a 24-hour hackathon hosted by the University of
+            Toledo, dedicated to fostering innovation and problem-solving among
+            students from the Midwest and beyond. This event will gather
+            talented students, from budding programmers to visionary designers,
+            to build real solutions to real-world challenges. With an emphasis
+            on collaboration, creativity, and technical skills, RocketHacks will
+            empower students to turn their ideas into impactful projects.
           </p>
         </div>
       </section>
