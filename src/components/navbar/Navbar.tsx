1467584242c89b59113c8e508293b5c37ca8390e
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-18 00:33:40 -0500

    Update Navbar to a fixed position on screen

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,9 +3,9 @@ import RocketHacks from "../../../public/assets/name-logo.svg";
 
 export default function Navbar() {
   return (
-    <nav className="bg-inherit text-white text-base">
+    <nav className="bg-[#030c1b] text-white text-base sticky top-0 z-20">
       {/* Logo Link */}
-      <div className="container ml-10 xl:ml-80 mt-3 mb-3 px-4 py-3 flex space-x-10 items-center h-10 mr-10">
+      <div className="container ml-10 xl:ml-80 mt-3 mb-3 px-4 py-3 flex space-x-10 items-center h-[3.8rem] mr-10">
         <div className="font-bold">
           <Link href="https://www.rockethacks.org">
             <RocketHacks className="h-16 w-auto" />
@@ -14,7 +14,9 @@ export default function Navbar() {
 
         {/* Navigation Links */}
         <div className="space-x-10 hidden xl:block">
-          <Link href="#about">About Us</Link>
+          <Link href="#about" style={{ scrollBehavior: "smooth" }}>
+            About Us
+          </Link>
           <Link href="/faq">FAQ</Link>
           <Link href="/sponsor">Sponsor</Link>
           <Link href="/contact">Contact Us</Link>
