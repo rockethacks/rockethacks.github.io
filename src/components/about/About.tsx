133c2556a67c91b7af697ba007f538e78e192d30
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-18 00:15:15 -0500

    Add about

---
+++ src/components/about/About.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <div>
+      <section id="about" className="about bg-white text-black py-16">
+        <div className="container mx-auto px-6">
+          <h2 className="text-3xl font-bold text-center">About Us</h2>
+          <p className="mt-4 text-lg text-center">
+            
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
