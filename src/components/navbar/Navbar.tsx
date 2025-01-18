9735d70c6b4614b68b3748dc82f6197d59ae8232
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-18 13:03:08 -0500

    Update Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -8,8 +8,8 @@ export default function Navbar() {
       <div className="container px-5 xl:px-96 flex space-x-10 items-center h-[3.8rem] mr-10">
         <div className="font-bold">
           <Link href="https://www.rockethacks.org">
-            <Image
-              src="/assets/name-logo.svg"
+            <img
+              src={`/assets/name-logo.svg`}
               alt="RocketHacks Logo"
               width={12}
               height={12}
