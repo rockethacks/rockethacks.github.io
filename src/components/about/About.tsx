10bdada9cc0b122a167993122d2051bce7a91935
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-18 01:15:15 -0500

    Update about styles

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function about() {
   return (
     <div>
-      <section id="about" className="bg-white text-black py-16 about-us">
+      <section
+        id="about"
+        className="about-us bg-white text-black py-16 px-5 md:px-10 xl:py-20 xl:px-[300px]"
+      >
         <div className="container text-left">
           <h2 className="text-3xl font-bold my-[10px]">ABOUT US</h2>
           <p className="mt-4 text-lg">
