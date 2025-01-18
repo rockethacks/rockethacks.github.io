95fe6ad1446de87e15cd5a45cca972e5c514f097
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-18 12:59:15 -0500

    Update Navbar RocketHacks Logo asset

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import RocketHacks from "../../../public/assets/name-logo.svg";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
@@ -8,7 +8,13 @@ export default function Navbar() {
       <div className="container px-5 xl:px-96 flex space-x-10 items-center h-[3.8rem] mr-10">
         <div className="font-bold">
           <Link href="https://www.rockethacks.org">
-            <RocketHacks className="h-16 w-auto" />
+            <Image
+              src="/assets/name-logo.svg"
+              alt="RocketHacks Logo"
+              width={12}
+              height={12}
+              className="h-16 w-auto"
+            />
           </Link>
         </div>
 
