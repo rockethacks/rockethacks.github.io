1b4ca33bd10a16afb087e857fe793bee73e61aa2
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-18 00:48:44 -0500

    Fix the background image styling for Mobile and Desktop Screens

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,12 +8,12 @@ export default function hero() {
     <div>
       <section
         id="home"
-        className="home text-white relative text-center h-screen"
+        className="home text-white relative text-center h-screen overflow-hidden"
       >
         <img
           src={"/assets/rocky.png"}
           alt="Rocky"
-          className="mx-auto opacity-90 absolute h-full w-full"
+          className="mx-auto opacity-90 absolute inset-0 h-full w-full object-cover md:object-fill"
         />
         <div className="absolute inset-0 flex flex-col items-center justify-center">
           <div>
@@ -25,6 +25,11 @@ export default function hero() {
             <h1 className="text-5xl">
               <span className="text-blue-700">{"{"}</span>RocketHacks
               <span className="text-blue-700">{"}"}</span>
+            </h1>
+          </div>
+          <div>
+            <h1 className={`${terminal.className} text-5xl uppercase mt-2`}>
+              The biggest Hackathon <br /> in the midwest
             </h1>
           </div>
         </div>
