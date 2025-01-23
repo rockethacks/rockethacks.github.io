bf3348fc4127547c9f81d45344d573ee5b56a34c
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-23 15:30:13 -0500

    Add Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar/Navbar";
 import Hero from "@/components/hero/Hero";
 import About from "@/components/about/About";
 import Faq from "@/components/faq/Faq";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <About />
       <Faq />
+      <Footer />
     </div>
   );
 }
