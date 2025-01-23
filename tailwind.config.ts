f489f90989a455d5ac4d26e7cda0593b68f613cd
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-23 15:03:55 -0500

    Add accordion to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,43 @@
+import {heroui} from '@heroui/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
     darkMode: ["class"],
-    content: [
-		"./src/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/globals.css", // Ensure this line is present
+    "./src/app/globals.css",
+    "// Ensure this line is present",
+    "./node_modules/@heroui/theme/dist/components/(accordion|divider).js"
   ],
    theme: {
-    extend: {},
-  },
-  plugins: [],
+   	extend: {
+   		keyframes: {
+   			'accordion-down': {
+   				from: {
+   					height: '0'
+   				},
+   				to: {
+   					height: 'var(--radix-accordion-content-height)'
+   				}
+   			},
+   			'accordion-up': {
+   				from: {
+   					height: 'var(--radix-accordion-content-height)'
+   				},
+   				to: {
+   					height: '0'
+   				}
+   			}
+   		},
+   		animation: {
+   			'accordion-down': 'accordion-down 0.2s ease-out',
+   			'accordion-up': 'accordion-up 0.2s ease-out'
+   		}
+   	}
+   },
+  plugins: [heroui()],
 };
 export default config;
