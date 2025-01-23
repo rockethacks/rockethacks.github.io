d5bbbee2a9ba6ea9d4f5b1ed37baf1bdc9f26778
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-23 16:52:24 -0500

    Add spons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import Navbar from "@/components/navbar/Navbar";
 import Hero from "@/components/hero/Hero";
 import About from "@/components/about/About";
 import Faq from "@/components/faq/Faq";
+import Sponsor from "@/components/sponsor/Sponsor";
 import Footer from "@/components/footer/Footer";
+
 
 export default function Home() {
   return (
@@ -12,6 +14,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Sponsor />
       <Faq />
       <Footer />
     </div>
