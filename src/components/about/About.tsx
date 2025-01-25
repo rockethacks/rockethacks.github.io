47f655354ca2d6924e02511d9d692318d28ce2c3
Aditya <37655818+adi-075@users.noreply.github.com>
2025-01-25 14:14:34 -0500

    Refactor the box component

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -4,9 +4,10 @@ import { IoDiamond } from "react-icons/io5";
 import { AiOutlineDiscord } from "react-icons/ai";
 import localFont from "next/font/local";
 import Link from "next/link";
+
 const terminal = localFont({ src: "../../app/fonts/terminal-grotesque.ttf" });
 
-export default function about() {
+export default function About() {
   return (
     <div>
       <section
@@ -19,95 +20,83 @@ export default function about() {
           >
             ABOUT US
           </h2>
-          <p className="mt-4 text-base text-pretty break-normal font-normal text-center">
+          <p className="mt-4 text-base text-pretty font-normal text-center">
             RocketHacks is a 24-hour hackathon hosted by the University of
             Toledo, dedicated to fostering innovation and problem-solving among
-            students from the Midwest and beyond.{" "}
-            <br className="inline-block xl:hidden" />
-            <br className="inline-block xl:hidden" /> This event will gather
+            students from the Midwest and beyond. This event will gather
             talented students, from budding programmers to visionary designers,
             to build real solutions to real-world challenges. With an emphasis
             on collaboration, creativity, and technical skills, RocketHacks will
             empower students to turn their ideas into impactful projects.
           </p>
-          <div className="flex flex-col lg:flex-row items-center justify-center align-middle text-base space-x-4">
-            <div className="border border-blue-600 border-2 mt-16 text-center">
-              <div className="px-8 py-8">
+          <div className="flex flex-col lg:flex-row items-stretch justify-center gap-4 mt-10 max-w-5xl mx-auto">
+            {/* Box 1 */}
+            <div className="flex-1 border-2 border-blue-600 rounded-lg text-center p-8 flex flex-col items-center justify-between">
+              <div>
                 <PiTerminalWindow
-                  className="mx-auto mb-2"
+                  className="mx-auto mb-4"
                   size={64}
                   color="#E4335E"
                 />
                 <h3
-                  className={`${terminal.className} mb-5 text-[#E4335E] text-[32px] `}
+                  className={`${terminal.className} mb-4 text-[#E4335E] text-[32px]`}
                 >
-                  Hackers
+                  HACKERS
                 </h3>
-                <p className="text-balance break-normal text-base">
-                  Applications for RocketHacks are now <br />
-                  open. Apply to be a hacker today!
+                <p className="text-base">
+                  Applications for RocketHacks are now open.&nbsp; Apply to be a
+                  hacker today!
                 </p>
-                <br />
-                <br />
-                <Link href="https://portal.rockethacks.org/">
-                  <button className="bg-blue-600 py-3 px-20 rounded-md">
-                    APPLY NOW
-                  </button>
-                </Link>
-                <br />
               </div>
+              <Link href="https://portal.rockethacks.org/">
+                <button className="bg-blue-600 py-3 px-20 rounded-md mt-6">
+                  APPLY NOW
+                </button>
+              </Link>
             </div>
-            <div className="border border-blue-600 border-2 mt-16 text-center">
-              <div className="px-11 py-5">
-                <IoDiamond
-                  className="mx-auto mb-2"
-                  size={64}
-                  color="#FFDA20"
-                ></IoDiamond>
+            {/* Box 2 */}
+            <div className="flex-1 border-2 border-blue-600 rounded-lg text-center p-8 flex flex-col items-center justify-between">
+              <div>
+                <IoDiamond className="mx-auto mb-4" size={64} color="#FFDA20" />
                 <h3
-                  className={`${terminal.className} mb-5 text-[#FFDA20] text-[32px] `}
+                  className={`${terminal.className} mb-4 text-[#FFDA20] text-[32px]`}
                 >
-                  Sponsors
+                  SPONSORS
                 </h3>
-                <p className="text-balance break-normal text-base">
-                  Are you interested in <br /> sponsoring RocketHacks? <br />{" "}
-                  Check out the sponsorship <br /> packet for more info!
+                <p className="text-base">
+                  Are you interested in sponsoring RocketHacks? Check out the
+                  sponsorship packet for more info!
                 </p>
-                <br />
-
-                <Link href="/documents/sponsorship-packet.pdf" target="_blank">
-                  <button className="bg-blue-600 py-3 px-20 rounded-md">
-                    SPONSOR US
-                  </button>
-                </Link>
-                <br />
               </div>
+              <Link href="/documents/sponsorship-packet.pdf" target="_blank">
+                <button className="bg-blue-600 py-3 px-20 rounded-md mt-6">
+                  SPONSOR
+                </button>
+              </Link>
             </div>
-            <div className="border border-blue-600 border-2 mt-16 text-center">
-              <div className="px-8 py-8">
+            {/* Box 3 */}
+            <div className="flex-1 border-2 border-blue-600 rounded-lg text-center p-8 flex flex-col items-center justify-between">
+              <div>
                 <AiOutlineDiscord
-                  className="mx-auto mb-2"
+                  className="mx-auto mb-4"
                   size={64}
                   color="#5865F2"
-                ></AiOutlineDiscord>
+                />
                 <h3
-                  className={`${terminal.className} mb-5 text-[#5865F2] text-[32px] `}
+                  className={`${terminal.className} mb-4 text-[#5865F2] text-[32px]`}
                 >
-                  Volunteer
+                  VOLUNTEER
                 </h3>
-                <p className="text-balance break-normal text-base">
-                  Do you want to volunteer at <br /> RocketHacks this year? Join
-                  the <br /> Discord Server
+                <p className="text-base">
+                  Do you want to volunteer at RocketHacks this year? Join the
+                  Discord Server.
                 </p>
-                <br />
-
-                <Link href="https://discord.gg/9NHTphRSUv">
-                  <button className="bg-blue-600 py-3 px-20 rounded-md">
-                    JOIN DISCORD
-                  </button>
-                </Link>
-                <br />
               </div>
+              <Link href="https://discord.gg/9NHTphRSUv">
+                <button className="bg-blue-600 py-3 px-20 rounded-md mt-6">
+                  JOIN
+                </button>
+              </Link>
             </div>
           </div>
         </div>
