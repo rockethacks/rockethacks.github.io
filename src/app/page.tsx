6df3ed1f415f14376a8c4be865173e615ea7df34
Aditya <37655818+adi-075@users.noreply.github.com>
2025-02-19 10:37:26 -0500

    Import SponsorData to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,100 +6,13 @@ import Hero from "@/components/hero/Hero";
 import About from "@/components/about/About";
 import Faq from "@/components/faq/Faq";
 import Sponsor from "@/components/sponsor/Sponsor";
+import { SponsorData } from "@/components/sponsor/SponsorData";
 import Footer from "@/components/footer/Footer";
 import Contact from "@/components/contact/Contact";
 // import Gallery from "@/components/gallery/Gallery";
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
-
-  const sponsors = [
-    {
-      type: "image",
-      src: "/assets/sponsors/prhi.png",
-      alt: "Pittsburgh Regional Health Initiative",
-      link: "https://prhi.org/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/spoke.png",
-      alt: "SpokeHQ",
-      link: "https://spokehq.com/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/Nysus-sol.png",
-      alt: "Nysus Solutions",
-      link: "https://www.nysus.com/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/actual-tech.png",
-      alt: "Actual Reality Technologies",
-      link: "https://www.actualreality.tech/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/Koolkat-Science.svg",
-      alt: "Koolkat Science",
-      link: "https://koolkatscience.net/",
-    },
-    {
-      type: "icon",
-      icon: "github",
-      alt: "GitHub",
-      link: "https://www.github.com/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/mercy.svg",
-      alt: "Mercy Health",
-      link: "https://www.mercy.com/",
-    },
-    { type: "icon", icon: "aws", alt: "AWS", link: "https://aws.amazon.com/" },
-    {
-      type: "image",
-      src: "/assets/sponsors/codeecho.svg",
-      alt: "Code Echo",
-      link: "https://codeecho.com/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/Perplexity.png",
-      alt: "Perplexity AI",
-      link: "https://www.perplexity.ai/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/warp.png",
-      alt: "Warp Terminal",
-      link: "https://www.warp.dev/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/Hugging-face.png",
-      alt: "Hugging Face",
-      link: "https://huggingface.co/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/xyz.svg",
-      alt: ".XYZ",
-      link: "https://www.gen.xyz/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/photoroom.svg",
-      alt: "Photoroom",
-      link: "https://www.photoroom.com/",
-    },
-    {
-      type: "image",
-      src: "/assets/sponsors/standout-stickers.svg",
-      alt: "Standout Stickers",
-      link: "https://www.standoutstickers.com/",
-    },
-  ];
 
   useEffect(() => {
     if (!localStorage.getItem("hasSeenAlert")) {
@@ -130,7 +43,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
-      <Sponsor sponsors={sponsors} />
+      <Sponsor sponsors={SponsorData} />
       {/* <Gallery /> */}
       <Contact />
       <Faq />
