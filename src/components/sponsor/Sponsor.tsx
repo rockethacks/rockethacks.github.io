2ec37a3e366b9715afd166c35e585229816c4a91
Aditya <37655818+adi-075@users.noreply.github.com>
2025-02-28 10:17:47 -0500

    Remove the white background from dept logos

--- src/components/sponsor/Sponsor.tsx
+++ src/components/sponsor/Sponsor.tsx
@@ -53,11 +53,7 @@ const Sponsor: React.FC<SponsorProps> = ({ sponsors }) => {
                     height={180}
                     className={`hover:scale-110 transition-transform duration-300 object-contain ${
                       sponsor.src.includes("xyz") ? "w-28 h-28" : ""
-                    } ${sponsor.src.includes("spoke") ? "bg-white p-3" : ""} ${
-                      sponsor.src.includes("College-of-engineering")
-                        ? "bg-white p-3"
-                        : ""
-                    } ${sponsor.src.includes("eecs") ? "bg-white p-3" : ""}`}
+                    } ${sponsor.src.includes("spoke") ? "bg-white p-3" : ""}`}
                   />
                 ) : sponsor.type === "icon" ? (
                   sponsor.icon === "github" ? (
@@ -88,11 +84,7 @@ const Sponsor: React.FC<SponsorProps> = ({ sponsors }) => {
                     height={180}
                     className={`hover:scale-110 transition-transform duration-300 object-contain ${
                       sponsor.src.includes("xyz") ? "w-28 h-28" : ""
-                    } ${sponsor.src.includes("spoke") ? "bg-white p-3" : ""}  ${
-                      sponsor.src.includes("College-of-engineering")
-                        ? "bg-white p-3"
-                        : ""
-                    } ${sponsor.src.includes("eecs") ? "bg-white p-3" : ""}`}
+                    } ${sponsor.src.includes("spoke") ? "bg-white p-3" : ""}`}
                   />
                 ) : sponsor.type === "icon" ? (
                   sponsor.icon === "github" ? (
