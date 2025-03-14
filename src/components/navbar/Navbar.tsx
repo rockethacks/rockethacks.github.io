ce5c8f646423e358d4d25cbe76f047adf5e53d2a
Aditya <37655818+adi-075@users.noreply.github.com>
2025-03-13 21:37:20 -0400

    Add transparency to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="bg-[#030c1b] text-white text-base sticky top-0 z-20 shadow-xl">
+    <nav className="bg-[#030c1b]/[0.97] backdrop-blur-sm text-white text-base sticky top-0 z-20 shadow-xl">
       {/* Logo and Hamburger Menu */}
       <div className="container flex justify-around sm:justify-between sm:mx-auto md:justify-center md:space-x-20 items-center h-[3.8rem]">
         {/* Logo */}
